98dd17e28632a3c4eb6e1db6be57793a8f0823a6
Savindu Supun <contact@supunsathsara.com>
2024-04-26 23:51:18 +0530

    Update Home Screen with lottie animations ✨

--- FishFeeder-App/index.js
+++ FishFeeder-App/index.js
@@ -5,11 +5,11 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import { PaperProvider } from 'react-native-paper';
+import { PaperProvider, MD3DarkTheme } from 'react-native-paper';
 
 export default function Main() {
     return (
-      <PaperProvider>
+      <PaperProvider theme={MD3DarkTheme}>
         <App />
       </PaperProvider>
     );
--- FishFeeder-App/src/screens/Home.tsx
+++ FishFeeder-App/src/screens/Home.tsx
@@ -1,26 +1,67 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
-import { Button, Text, Provider as PaperProvider } from 'react-native-paper';
+import { SafeAreaView, StyleSheet, View, useWindowDimensions } from 'react-native';
+import { Button, Text, Provider as PaperProvider, FAB, Card, Title, Snackbar } from 'react-native-paper';
+import LottieView from 'lottie-react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const HomeScreen = () => {
+  const { width, height } = useWindowDimensions();
+  const [visible, setVisible] = useState(false);
+
+  const onDismissSnackBar = () => setVisible(false);
+
   const [nextFeedTime, setNextFeedTime] = useState('12:00 PM');
 
+  const handleFeedNow = () => {
+    console.log('Feed now');
+    //! Add logic to feed the fish
+
+    setVisible(true);
+
+  }
+
   return (
-    <PaperProvider >
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.title}>Fish Feeder</Text>
-        <View style={styles.section}>
-          <Text style={styles.subtitle}>Next Feeding Time:</Text>
-          <Text style={styles.info}>{nextFeedTime}</Text>
-          <Button mode="contained" onPress={() => console.log('Feed now')}>
-            Feed Now
-          </Button>
-          <Button mode="contained" onPress={() => console.log('Set Schedule')}>
-            Set Schedule
-          </Button>
-        </View>
-      </SafeAreaView>
-    </PaperProvider>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title} variant="titleLarge">Fish Feeder</Text>
+      <LottieView style={{ width: width * 0.8, height: height * 0.3 }} source={require('../assets/animations/fish.json')} autoPlay loop />
+      <View style={styles.section}>
+
+      <Card style={styles.card}>
+          <Card.Content>
+            <Icon name="clock" size={24} color="#fff" style={styles.icon} />
+            <Text style={styles.subtitle}>Next Feeding Time:</Text>
+            <Text style={styles.info}>{nextFeedTime}</Text>
+          </Card.Content>
+        </Card>
+        <FAB
+          variant='primary'
+          icon='food'
+          style={styles.button}
+          color='#fff'
+          label='Feed Now'
+          onPress={
+            handleFeedNow
+          } />
+        <FAB
+          variant='secondary'
+          icon='calendar-clock'
+          style={styles.button}
+          color='#fff'
+          label='schedule'
+          onPress={() => console.log('Schedule feed')} />
+           <Snackbar
+        visible={visible}
+        onDismiss={onDismissSnackBar}
+        action={{
+          label: 'Okay',
+          onPress: () => {
+            setVisible(false);
+          },
+        }}>
+        Fish fed successfully!
+      </Snackbar>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -36,6 +77,7 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontWeight: 'bold',
     marginBottom: 20,
+    textAlign: 'center',
   },
   section: {
     width: '80%',
@@ -45,12 +87,30 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: '#fff',
     marginBottom: 10,
+    textAlign: 'center',
   },
   info: {
     fontSize: 16,
     color: '#fff',
     marginBottom: 20,
-  }
+    textAlign: 'center',
+  },
+  button: {
+    marginBottom: 10,
+    backgroundColor: '#6B4FA9',
+  },
+  card: {
+    width: '80%',
+    marginBottom: 20,
+    backgroundColor: '#1A1A1A',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    alignSelf: 'center',
+  },
+  icon: {
+    alignSelf: 'center',
+    marginBottom: 10,
+  },
 });
 
 export default HomeScreen;
