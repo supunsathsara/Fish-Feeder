b2d3a92056ae9aa05f7a11fd340b6f2df26b9430
Savindu Supun <contact@supunsathsara.com>
2024-05-30 03:03:59 +0530

    Add fonts for vector-icons ✨

--- FishFeeder-App/babel.config.js
+++ FishFeeder-App/babel.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['module:react-native-dotenv']
+  ],
   env: {
     production: {
-      plugins: ['react-native-paper/babel'],
+      plugins: ['react-native-paper/babel','module:react-native-dotenv'],
     },
   },
 };
